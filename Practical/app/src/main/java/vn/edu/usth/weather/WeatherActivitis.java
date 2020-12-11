b705f56bc2281568bba9b182ffc24dc9e1dc0df6
Syrus123 <39728391+Syrus123@users.noreply.github.com>
2020-12-11 11:36:14 +0700

    Labwork1
    Practical1-2-3. 4 is not done yet!

---
+++ Practical/app/src/main/java/vn/edu/usth/weather/WeatherActivitis.java
@@ -0,0 +1,46 @@
+package vn.edu.usth.weather;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+public class WeatherActivitis extends AppCompatActivity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("MainActivity Lifecycle", "===== onCreate =====");
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("MainActivity Lifecycle", "===== onStart =====");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("MainActivity Lifecycle", "===== onRestart =====");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("MainActivity Lifecycle", "===== onResume =====");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("MainActivity Lifecycle", "===== onPause =====");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("MainActivity Lifecycle", "===== onStop =====");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("MainActivity Lifecycle", "===== onDestroy =====");
+    }
+}
