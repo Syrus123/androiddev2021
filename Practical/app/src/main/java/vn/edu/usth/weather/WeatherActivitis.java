9954f9cc52a3d4c93a218ed609824f6bcc8e3665
Syrus123 <39728391+Syrus123@users.noreply.github.com>
2021-01-11 23:44:08 +0700

    Prac8

--- Practical/app/src/main/java/vn/edu/usth/weather/WeatherActivitis.java
+++ Practical/app/src/main/java/vn/edu/usth/weather/WeatherActivitis.java
@@ -1,5 +1,4 @@
 package vn.edu.usth.weather;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,9 +6,9 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
 import android.os.Bundle;
 import android.util.Log;
+import com.google.android.material.tabs.TabLayout;
 public class WeatherActivitis extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +20,14 @@ public class WeatherActivitis extends AppCompatActivity {
         pager.setAdapter(adapter);
 
         Log.i("create", "onCreate: ");
-    }
 
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(pager);
+    }
      protected void onDestroy() {
         super.onDestroy();
         Log.i("destroy", "onDestroy: ");
     }
-
     public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
         private final int PAGE_COUNT = 3;
         private String titles[] = new String[] {"Ha Noi", "Paris", "Toulouse"};
@@ -54,7 +54,6 @@ public class WeatherActivitis extends AppCompatActivity {
                     return new WeatherAndForecast();
             }
         }
-
         @Override
         public CharSequence getPageTitle(int page) {
             return titles[page];
