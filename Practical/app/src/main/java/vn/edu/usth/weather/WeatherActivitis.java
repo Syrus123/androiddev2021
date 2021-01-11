254a62fdfb569e962c8f9689c772022a67d68aee
Syrus123 <39728391+Syrus123@users.noreply.github.com>
2021-01-11 23:00:20 +0700

    Prac7

--- Practical/app/src/main/java/vn/edu/usth/weather/WeatherActivitis.java
+++ Practical/app/src/main/java/vn/edu/usth/weather/WeatherActivitis.java
@@ -1,45 +1,63 @@
 package vn.edu.usth.weather;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
 import android.os.Bundle;
 import android.util.Log;
 public class WeatherActivitis extends AppCompatActivity {
     @Override
-    protected void onCreate (Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_activitis);
+        PagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+        pager.setAdapter(adapter);
 
-        Log.i("MainActivity Lifecycle", "===== onCreate =====");
+        Log.i("create", "onCreate: ");
+    }
 
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("MainActivity Lifecycle", "===== onStart =====");
-    }
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i("MainActivity Lifecycle", "===== onRestart =====");
-    }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("MainActivity Lifecycle", "===== onResume =====");
-    }
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Log.i("MainActivity Lifecycle", "===== onPause =====");
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("MainActivity Lifecycle", "===== onStop =====");
-    }
-    @Override
-    protected void onDestroy() {
+     protected void onDestroy() {
         super.onDestroy();
-        Log.i("MainActivity Lifecycle", "===== onDestroy =====");
+        Log.i("destroy", "onDestroy: ");
+    }
+
+    public class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private final int PAGE_COUNT = 3;
+        private String titles[] = new String[] {"Ha Noi", "Paris", "Toulouse"};
+
+        public HomeFragmentPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return PAGE_COUNT;
+        }
+
+        @Override
+        public Fragment getItem(int page) {
+            switch (page) {
+                case 0:
+                    return new WeatherAndForecast();
+                case 1:
+                    return new WeatherAndForecast();
+                case 2:
+                    return new WeatherAndForecast();
+                default:
+                    return new WeatherAndForecast();
+            }
+        }
+
+        @Override
+        public CharSequence getPageTitle(int page) {
+            return titles[page];
+        }
     }
 }
